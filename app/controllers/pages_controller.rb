380a54c6a439693d70b43a59d8e4d61e4eff4a2c
Karmen de Reuck <dereuckkarmen@gmail.com>
2023-03-06 13:39:29 +0200

    stupid coaching

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+  def ask
+    # @answer = params[:answer]
+  end
+
+  def answer
+    if params[:question] == 'I am going to work'
+      @answer = 'Great'
+    elsif params[:question].ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
